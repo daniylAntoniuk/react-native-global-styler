b3d32b6ce3bb3939b8ee6e82cb85b8888eacfa57
Daniyl <34713549+daniylAntoniuk@users.noreply.github.com>
2025-07-02 23:05:21 -0500

    Fix

---
+++ index.js
@@ -0,0 +1,4 @@
+export {default as GSView} from './src/componets/View'; 
+export {default as View} from './src/componets/View'; 
+
+export {default as useGlobalStyler} from './src/styles/useGlobalStyler'; 
