5a01672d337a2cb217502b17f1b0c2744e5c8f0a
Daniyl <34713549+daniylAntoniuk@users.noreply.github.com>
2025-07-02 23:03:53 -0500

    Fix

--- src/componets/View.tsx
+++ src/componets/View.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import { View, ViewProps } from 'react-native';
+import useGlobalStyler from '../styles/useGlobalStyler';
+import BaseGlobalStyles from '../styles/types/BaseGlobalStyles';
 
-type GSViewProps = ViewProps;
+type GSViewProps = BaseGlobalStyles & ViewProps;
 
 const GSView = (props: GSViewProps) => {
-    
+    const generatedStyles = useGlobalStyler(props);
     return (
-        <View {...props} style={{}}>
+        <View {...props} style={[generatedStyles, props.style]}>
             {props.children}
         </View>
     )
